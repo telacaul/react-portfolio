d251ddffe151c2eaca551399fa3259a3c9ccf3f3
Tela Caul <telacaul@gmail.com>
2021-06-22 20:02:45 -0700

    reconfigure nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
@@ -9,10 +9,6 @@ function Nav(props) {
     currentCategory,
     setContactSelected,
   } = props;
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
