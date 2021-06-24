451f328e1952e16f017eb7c3d378c90eacdd3553
Tela Caul <telacaul@gmail.com>
2021-06-24 11:21:01 -0700

    about page and contact page reorg

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+import React from 'react';
+
 
 function About() {
 
-  return (
-    <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
-      </div>
-    </section>
-  )
+    return (
+        <>
+            <section className="columns text-center" id="about">
+                <div className="row justify-content-center">
+                    <div className="column col-6">
+                        <img src="./assets/img/graduation.JPG" className="img-fluid rounded" alt="graduation"></img>
+                        <p>SF State Graduation 2016</p>
+                    </div>
+                    <div className="column col-6">
+                        <h4 id="about-me">About Me</h4>
+                        <p>Hi! My name is Richard Brian Santos and I am a graduate from San Francisco State University
+                        with a bachelors in Anthropology. I am at my happiest when I am learning and I am very
+                        passionate about technology. Coding is very fascinating, challenging and a rewarding experience. I cannot wait to learn more and enhance my coding
+                            skills!</p>
+                    </div>
+                </div>
+            </section>
+            <section className="columns text-center" id="passions">
+                <div className="row justify-content-center">
+                    <div className="column col-6">
+                        <h4 id="love">What I love</h4>
+                        <p>On my free time, I love hanging out with my family and friends. If it is just hanging out at
+                        home or going out to eat, I will always make time for them. I also love to travel and I hope
+                        to travel to places that I havn't been before such as, Europe and other parts of Asia. So
+                        far I have been to Mexico, Canada, Japan, and Thailand. Cannot wait for the next adventure!
+                        </p>
+                    </div>
+                    <div className="column col-6">
+                        <img src="./assets/img/hiking.jpeg" className="img-fluid rounded" alt="bigBasin"></img>
+                        <p>Big Basin</p>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
 }
 
-export default About
+
+export default About;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,66 +1,77 @@
 import React, { useState } from 'react';
-
 import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // enter JSX here
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid!');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required!`);
+            } else {
+                setErrorMessage('');
+            }
+        }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+
     }
-  };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
     }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
 
-  return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
-    </section>
-  );
+    return (
+        <form id="contact-form" onSubmit={handleSubmit}>
+            <h2>Connect with me!</h2>
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input 
+                type="text" 
+                name="name" 
+                defaultValue={name} 
+                onBlur={handleChange} 
+                className="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="emailHelp" 
+                placeholder="Enter email">
+                </input>
+            </div>
+            <br />
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" defaultValue={email} onBlur={handleChange} className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            </div>
+            <br />
+            <div class="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" defaultValue={message} onBlur={handleChange} className="form-control" id="exampleFormControlTextarea1" rows="6"></textarea>
+            </div>
+            <br />
+            {errorMessage && (
+                  <div>
+                      <p className="error-text">{errorMessage}</p>
+                  </div>
+            )}
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
 }
+
 
 export default ContactForm;
