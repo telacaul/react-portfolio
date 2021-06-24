33536e9301ed7d852801904e0275598952c3619b
Tela Caul <telacaul@gmail.com>
2021-06-24 11:28:23 -0700

    hero file and styling

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <section className="jumbotron col-12">
+                <img src="./assets/img/outdoors.jpeg" className="img-fluid rounded" id="selfie" alt="Yosemite"></img>
+        </section>
+    )
+}
+
+
+export default Hero;
