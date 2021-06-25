25e95669d0cbcd15a6b4ba3db082f4f3d971cadc
Tela Caul <telacaul@gmail.com>
2021-06-24 20:38:35 -0700

    about me style adjustments and bulma nomenclature

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,24 +9,25 @@ function About() {
                 <div className="row justify-content-center">
                     <div className="column col-6">
                         <img src="./assets/img/classroom.jpg" className="img-fluid rounded" alt="Teacher"></img>
-                        <p>My classroom at Making Waves Academy in Richmond, CA.</p>
+                        <p class="subtitle">My classroom at Making Waves Academy in Richmond, CA.</p>
                     </div>
                     <div className="column col-6">
                         <h4 class="title" id="about-me">About Me</h4>
-                        <p class="subtitle">Welcome! My name is Tela Caul and I am a passionate high school educator with a love for educational technology and its relationship to equity, access and differentiation in the classroom.</p>
+                        <p>Welcome! My name is Tela Caul and I am a passionate high school educator with a love for educational technology and its relationship to equity, access and differentiation in the classroom. </p>
+                        <p> I have an undergraduate degree in Human Physiology, minoring in Spanish, Biology and Chemistry at the University of Oregon Honors College. I also earned my masters in Urban Education focusing on Digital Learning.</p>
                     </div>
                 </div>
             </section>
             <section className="columns text-center" id="passions">
                 <div className="row justify-content-center">
                     <div className="column col-6">
-                        <h4 class="title" id="love">What I love</h4>
-                        <p class="subtitle">In my free time, I enjoy traveling and spending time in nature with my two Miniature Australian Shepards and my partner Jack.
+                        <h4 class="title" id="love">More About Me</h4>
+                        <p>In my free time, I enjoy traveling and spending time in nature with my two Miniature Australian Shepards and my partner Jack.
                         </p>
                     </div>
                     <div className="column col-6">
                         <img src="./assets/img/oregon.jpg" className="img-fluid rounded" alt="CraterLake"></img>
-                        <p>Crater Lake</p>
+                        <p class="subtitle">Crater Lake in Oregon with my partner and our puppy Eevee</p>
                     </div>
                 </div>
             </section>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Hero() {
     return (
-        <section className="jumbotron col-12">
-                <img src="./assets/img/outdoors.jpg" className="img-fluid rounded" id="selfie" alt="Yosemite"></img>
+        <section class="hero" className="jumbotron col-12">
+                <img class="hero-body" src="./assets/img/outdoors.jpg" className="img-fluid rounded" id="selfie" alt="Yosemite"></img>
         </section>
     )
 }
