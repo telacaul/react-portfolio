9bb10162734eade395f78e89cc058c2b76c7947c
Tela Caul <telacaul@gmail.com>
2021-07-12 08:51:39 -0700

    links to github projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,7 +6,7 @@ function Project() {
         <>
             <h2 className="justify-content-center text-center" id="projects">Projects</h2>
             <section class="col-12">
-                <a href="https://brewmap3.herokuapp.com/">
+                <a href="https://brewmap3.herokuapp.com/">BrewMaps
                     <img src="./assets/img/brewhome.jpg" class="img-fluid rounded" alt="BrewMaps"></img>
                 </a>
             </section>
@@ -14,12 +14,12 @@ function Project() {
             <section class="columns">
                 <div class="row justify-content-center">
                     <div class="column col-6">
-                        <a href="https://github.com/telacaul/note-taker">
+                        <a href="https://github.com/telacaul/note-taker">Note-Taker
                             <img src="./assets/img/note-taker.jpg" class="img-fluid rounded" alt="NoteTaker"></img>
                         </a>
                     </div>
                     <div class="column col-6">
-                        <a href="https://github.com/telacaul/mvc-tech-blog">
+                        <a href="https://github.com/telacaul/mvc-tech-blog">Tech Blog
                             <img src="./assets/img/tech-blog.jpg" class="img-fluid rounded" alt="TechBlog"></img>
                         </a>
                     </div>
@@ -29,12 +29,12 @@ function Project() {
             <section class="columns">
                 <div class="row justify-content-center">
                     <div class="column col-6">
-                        <a href="https://telacaul.github.io/weather_dashboard/">
+                        <a href="https://telacaul.github.io/weather_dashboard/">Weather Dashboard
                             <img src="./assets/img/weather.jpg" class="img-fluid rounded" alt="WeatherDashboard"></img>
                         </a>
                     </div>
                     <div class="column col-6">
-                        <a href="https://github.com/telacaul/pwa-budget-tracker">
+                        <a href="https://github.com/telacaul/pwa-budget-tracker">Budget Tracker
                             <img src="./assets/img/budget-tracker.jpg" class="img-fluid rounded" alt="BudgetTracker"></img>
                         </a>
                     </div>
@@ -44,7 +44,7 @@ function Project() {
             <section class="columns">
                 <div class="row justify-content-center">
                     <div class="column col-6">
-                        <a href="https://github.com/telacaul/js_password_generator">
+                        <a href="https://github.com/telacaul/js_password_generator">Password Generator
                             <img src="./assets/img/password.jpg" class="img-fluid rounded" alt="PasswordGenerator"></img>
                         </a>
                     </div>
